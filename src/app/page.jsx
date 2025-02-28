c4f150fcaa22deae2341d72cf04f9ea3ca6e2f3b
Neeraj Krishna <contact@neerajx0.xyz>
2025-03-01 01:24:16 +0530

    [style]: adjust background overlay opacity and clean up unused styles

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,7 +9,7 @@ import Carousel from "@/components/Artist"
 export default function Home() {
   return (
     <div
-      className="relative w-full flex flex-col gap-10 w-full items-center flex-grow min-h-screen"
+      className="relative flex flex-col gap-10 w-full items-center flex-grow min-h-screen"
       style={{
         backgroundImage: "url('/bg.svg')",
         backgroundSize: "cover",
@@ -24,9 +24,7 @@ export default function Home() {
             "url('https://res.cloudinary.com/dguddaxjl/image/upload/v1740741407/media-uploads/1740741407033-sg7bzdv526.png')",
         }} />
 
-      <div className="absolute w-full h-full z-0 bg-black/60" style={{
-        backgroundImage: "url('/black-overlay.png')",
-      }} />
+      <div className="absolute w-full h-full z-0 bg-black/10" />
 
       <NavBar />
       <Welcome />
