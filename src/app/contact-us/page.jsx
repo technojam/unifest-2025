c1131e237a86d7e5ed4e71f2a3b509c959b8dd47
avinasdube <avidubey712@gmail.com>
2025-03-01 02:22:36 +0530

    [configuration]: configured layout and removed redundancy

---
+++ src/app/contact-us/page.jsx
@@ -0,0 +1,10 @@
+import Heading from "@/components/Heading";
+
+
+export default function Home(){
+    return (
+      <div className="flex flex-col gap-10 w-full items-center flex-grow min-h-screen mt-80">
+        <Heading size="0xl">Contact Details</Heading>
+      </div>
+    );
+  };
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./styles/globals.css";
+import NavBar from "@/components/NavBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,8 +21,20 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={`${geistSans.variable} ${geistMono.variable} antialiased relative flex flex-col gap-10 w-full items-center flex-grow min-h-screen`}
+        style={{
+          backgroundImage: "url('/bg.svg')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}>
+        <div
+          className="absolute w-full h-[900px] bg-cover bg-center bg-no-repeat flex items-center justify-center z-10"
+          style={{
+            backgroundImage:
+              "url('https://res.cloudinary.com/dguddaxjl/image/upload/v1740741407/media-uploads/1740741407033-sg7bzdv526.png')",
+          }} />
+        <NavBar />
         {children}
       </body>
     </html>
