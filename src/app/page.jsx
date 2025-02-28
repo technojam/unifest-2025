5de7c12e6f5d02daa289a61619072536cfeb60e9
abhishek-7081 <luckydwivedi7081@gmail.com>
2025-02-28 13:12:03 +0530

    start orgainsing team work

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import NavBar from "@/components/NavBar";
+import Orgateam from "@/components/orgateam";
 export default function Home() {
   return (
     <div className="flex flex-col items-center min-h-screen py-2">
       <NavBar />
+      <Orgateam/>
     </div>
   );
 }
---
+++ src/components/orgateam.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+
+const Orgateam = () => {
+  return (
+    <>
+<div className="img23">
+
+  <img className='img123' src="./assests/Vector.jpg" alt="" />
+
+
+    <div className='cw'>Organising Team </div>
+    <div>
+    The Galgotias University Student Council is a vibrant community of around 11,000 members, bringing students together to make campus life exciting and meaningful. With 25+ clubs covering various interests and domains, it provides endless opportunities to explore passions, develop skills, and collaborate. Acting as a bridge between students and the administration, the Council ensures every voice is heard while creating a supportive and engaging environment for all.
+    </div>
+   </div>
+   
+    </>
+  )
+}
+
+export default Orgateam
