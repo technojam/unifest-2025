5dd30d14fc806e2657b1b16ae118f0f815c4e5a3
avinasdube <avidubey712@gmail.com>
2025-03-01 00:32:48 +0530

    [refactored]: refactored code and solved alignment issue

--- src/components/orgateam.jsx
+++ src/components/orgateam.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-import "../app/styles/orgateam.css"; 
+import "../app/styles/orgateam.css";
+import Heading from "./Heading";
+import Paragraph from "./Paragraph";
 
 const Orgateam = () => {
   return (
-    <div className="relative w-full min-h-[300px] bg-cover bg-center text-white p-8 md:p-12 
-    ">
-      <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-[#D69924] to-yellow-400 text-7xl md:text-7xl font-bold text-center uppercase tracking-wider drop-shadow-xl font-alinore ">
-        Organising Team
-      </h1>
+    <div className="w-full min-h-[300px] bg-cover bg-center text-white p-8 md:p-12">
+      <Heading>Organizing Team</Heading>
 
-      <p className="mt-6 text-center max-w-8/10 mx-auto text-xl font-light leading-relaxed 
-      drop-shadow-xl font-montesaro">
+      <Paragraph align="left">
         The Galgotias University Student Council is a vibrant community of
         around 11,000 members, bringing students together to make campus life
         exciting and meaningful. With 25+ clubs covering various interests and
@@ -18,7 +16,7 @@ const Orgateam = () => {
         skills, and collaborate. Acting as a bridge between students and the
         administration, the Council ensures every voice is heard while creating
         a supportive and engaging environment for all.
-      </p>
+      </Paragraph>
     </div>
   );
 };
