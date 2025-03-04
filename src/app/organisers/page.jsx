f78d5a065423c8f507e130647711cd65b6e7ad5f
priya <priyaaashu4@gmail.com>
2025-03-04 20:00:45 +0530

    Resolve button click issue in CoreTeam component

--- src/app/organisers/page.jsx
+++ src/app/organisers/page.jsx
@@ -9,7 +9,7 @@ export default function CoreTeam() {
   return (
     <div className="flex flex-col items-center w-full relative min-h-screen ">
       {/* Background Image */}
-      <div className="absolute top-0 left-0 right-0 z-0 h-screen overflow-hidden pointer-events-none ">
+      <div className="absolute top-0 left-0 right-0 h-screen overflow-hidden pointer-events-none ">
         <Image
           src="/bgyellow.webp"
           fill={true}
@@ -21,20 +21,23 @@ export default function CoreTeam() {
       </div>
 
       {/* Heading Section */}
-      <div className="w-full flex flex-col items-center pt-[20vh] pb-[30vh] md:pt-[30vh] lg:pt-[390px] z-10">
+      <div className="w-full flex flex-col items-center pt-[20vh] pb-[30vh] md:pt-[30vh] lg:pt-[390px] ">
         <h2 className="font-[Alinore] text-[#CC200E] font-bold text-center uppercase tracking-wider drop-shadow-[-6px_-2px_0px_black] text-4xl sm:text-5xl md:text-6xl lg:text-8xl px-4 ">
           ORGANISING TEAM
         </h2>
 
         {/* Tabs Section */}
-        <div className="flex w-[90%] max-w-[73vh] sm:w-[50%] md:w-[45%] h-[40px] rounded-[39px] bg-[#E5B73B] font-poppins overflow-hidden border border-black mt-4">
+        <div className="flex w-[90%] max-w-[73vh] sm:w-[50%] md:w-[45%] h-[40px] rounded-[39px] bg-[#E5B73B] font-poppins overflow-hidden border border-black mt-4 z-50 pointer-events-auto ">
           <button
             className={`flex-1 text-center py-2 rounded-[73.8px] font-bold text-sm sm:text-base transition-all duration-300 ${
               activeTab === "core"
                 ? "bg-[#A50C00] text-white"
                 : "bg-[#E5B73B] text-black"
             }`}
-            onClick={() => setActiveTab("core")}
+            onClick={() => {
+    
+    setActiveTab("core");
+  }}
           >
             CORE TEAM
           </button>
