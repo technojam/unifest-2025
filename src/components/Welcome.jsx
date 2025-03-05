ac1528903310180e0fe237a499a5674586275c2a
avinasdube <avidubey712@gmail.com>
2025-03-05 11:06:10 +0530

    Fix: Update Welcome component layout and image sizes for better responsiveness

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 const Welcome = () => {
     return (
-        <div className="flex justify-center items-center w-full h-[1000px] mt-30 z-10">
-            <img src="/vercel.svg" alt="Logo" className="w-220 h-220" />
+        <div className="flex justify-center items-center w-full h-fit-content sm: mt-40 md:mt-50 z-10">
+            <img
+                src="/vercel.svg"
+                alt="Logo"
+                className="w-120 h-120 sm:w-180 sm:h-180 md:w-200 md:h-200"
+            />
         </div>
     );
 };
